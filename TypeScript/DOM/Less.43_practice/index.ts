a27ba909c7d4c1c31d23da4b6152ee0794e85842
samsak1990 <samsak.developer@internet.ru>
2025-03-04 21:30:05 +0300

    Less 6_Practice

---
+++ TypeScript/DOM/Less.43_practice/index.ts
@@ -0,0 +1,118 @@
+
+// // Последовательность действий:
+// // 1) Происходит submit любой из форм
+// // 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
+// // 3) Запускается функция validateFormData с этим объектом, возвращает true/false
+// // 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+
+const forms = document.querySelectorAll('form');
+const title = document.querySelector('#title') as HTMLInputElement;
+const text = document.querySelector('#text') as HTMLInputElement;
+const email = document.querySelector('#email') as HTMLTextAreaElement;
+const checkbox = document.querySelector("#checkbox") as HTMLInputElement;
+
+interface IFormData {
+  email: string;
+  title: string;
+  text: string;
+  checkbox: boolean;
+}
+
+const formData: IFormData = {
+  email: "",
+  title: "",
+  text: "",
+  checkbox: false,
+};
+
+forms.forEach(form => {
+  form.addEventListener('submit', (e: SubmitEvent)=>{
+    e.preventDefault()
+    formData.email = email?.value ?? ''
+    formData.title = title?.value ?? ''
+    formData. text = text?.value ?? ''
+    formData.checkbox = checkbox?.checked ?? false
+    if (validateFormData(formData)) {
+      checkFormData(formData)
+    }
+  })
+  
+})
+
+
+function validateFormData(data: IFormData): boolean {
+  if (Object.values(data).every(value => value)) {
+    return true;
+  } else {
+    console.log("Please, complete all fields");
+    return false;
+  }
+}
+
+function checkFormData(data: IFormData) {
+  const { email } = data;
+  const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+  if (emails.includes(email)) {
+    console.log("This email is already exist");
+  } else {
+    console.log("Posting data...");
+  }
+}
+
+
+
+// const forms = document.querySelectorAll("form");
+// const email = document.querySelector("#email") as HTMLInputElement;
+// const title = document.querySelector("#title") as HTMLInputElement;
+// const text = document.querySelector("#text") as HTMLTextAreaElement;
+// const checkbox = document.querySelector("#checkbox") as HTMLInputElement;
+
+// interface IFormData {
+// 	email: string;
+// 	title: string;
+// 	text: string;
+// 	checkbox: boolean;
+// }
+
+// const formData: IFormData = {
+// 	email: "",
+// 	title: "",
+// 	text: "",
+// 	checkbox: false,
+// };
+
+// forms.forEach((form) =>
+// 	form.addEventListener("submit", (e) => {
+// 		e.preventDefault();
+
+// 		// Можно и создавать другой объект для соблюдения иммутабельности, но пока не обязательно
+// 		formData.email = email?.value ?? "";
+// 		formData.title = title?.value ?? "";
+// 		formData.text = text?.value ?? "";
+// 		formData.checkbox = checkbox?.checked ?? false;
+
+// 		if (validateFormData(formData)) {
+// 			checkFormData(formData);
+// 		}
+// 	})
+// );
+
+// function validateFormData(data: IFormData): boolean {
+// 	// Если каждое из свойств объекта правдиво...
+// 	if (Object.values(data).every((value) => value)) {
+// 		return true;
+// 	} else {
+// 		console.log("Please, complete all fields");
+// 		return false;
+// 	}
+// }
+
+// function checkFormData(data: IFormData) {
+//   const { email } = data;
+//   const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+//   if (emails.includes(email)) {
+//     console.log("This email is already exist");
+//   } else {
+//     console.log("Posting data...");
+//   }
+// }
---
+++ TypeScript/DOM/Less.43_practice/test.js
@@ -0,0 +1,3 @@
+const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+
+console.log(emails.includes("example@gail.com"))
